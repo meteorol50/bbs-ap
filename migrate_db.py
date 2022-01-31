7b5ab7582fdeea244b9fc031ea41757486f5ffe6
meteorol50 <meteorol50.sky@gmail.com>
2022-01-31 15:45:19 +0900

    ディレクトリ名変更等軽微な修正

--- migrate_db.py
+++ migrate_db.py
@@ -7,9 +7,9 @@ engine = create_engine(DB_URL, echo=True)
 
 
 def reset_database():
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
+  Base.metadata.drop_all(bind=engine)
+  Base.metadata.create_all(bind=engine)
 
 
 if __name__ == "__main__":
-    reset_database()
+  reset_database()
